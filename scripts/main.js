99ec7bb7a5139eed1209f85bdf5417595a7f4dca
jonadls <jona13_metalml@hotmail.com>
2025-07-07 19:51:51 -0300

    avanzamos con la pagina, aun en la pagina inicio, añadimos css, vectores de js para movimientos y un backgroud

---
+++ scripts/main.js
@@ -0,0 +1,43 @@
+// Esperar que el DOM esté cargado (opcional)
+document.addEventListener('DOMContentLoaded', () => {
+    particlesJS("particles-js", {
+        particles: {
+        number: { value: 80, density: { enable: true, value_area: 800 } },
+        color: { value: "#e40000" },
+        shape: { type: "circle" },
+        opacity: {
+            value: 0.5,
+            random: true
+        },
+        size: {
+            value: 3,
+            random: true
+        },
+        line_linked: {
+            enable: true,
+            distance: 150,
+            color: "#ffffff",
+            opacity: 0.4,
+            width: 1
+        },
+        move: {
+            enable: true,
+            speed: 2,
+            direction: "none",
+            out_mode: "out"
+        }
+        },
+        interactivity: {
+        detect_on: "canvas",
+        events: {
+            onhover: { enable: true, mode: "grab" },
+            onclick: { enable: true, mode: "push" }
+        },
+        modes: {
+            grab: { distance: 200, line_linked: { opacity: 0.5 } },
+            push: { particles_nb: 4 }
+        }
+        },
+        retina_detect: true
+    });
+});
